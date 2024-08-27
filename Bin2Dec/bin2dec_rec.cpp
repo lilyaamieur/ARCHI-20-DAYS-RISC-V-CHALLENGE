278384149d9a5597272798bd38655af0ab5d5bb2
Lilya Amieur <146237373+lilyaamieur@users.noreply.github.com>
2024-08-27 04:24:57 +0100

    Update bin2dec_rec.cpp

--- Bin2Dec/bin2dec_rec.cpp
+++ Bin2Dec/bin2dec_rec.cpp
@@ -1,1 +1,23 @@
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int toDecimal(string binary, int i=0)
+{
+    // If we reached last character
+    int n = binary.length();
+    if (i == n-1)
+      return binary[i] - '0';
+     
+    // Add current tern and recur for
+    // remaining terms
+    return ((binary[i] - '0') << (n-i-1))  +
+            toDecimal(binary, i+1);
+}
+ 
+// Driver code
+int main()
+{
+    string binary = "1010";
+    cout << toDecimal(binary) << endl;
+    return 0;
+}
