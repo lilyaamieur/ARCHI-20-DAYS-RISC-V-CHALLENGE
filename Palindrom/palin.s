0fd0ae870981794b813d55d28eeec0abc006b930
Lilya Amieur <146237373+lilyaamieur@users.noreply.github.com>
2024-08-27 08:33:46 +0100

    Update palin.s

--- Palindrom/palin.s
+++ Palindrom/palin.s
@@ -1,1 +1,42 @@
+.global _start
 
+.org 0x200
+
+.data
+palin: .ascii "lil"
+
+.text
+
+_start:
+    la a0, palin
+    li a1, 15         #length of the string
+    jal ra, palindrom
+
+palindrom:
+    li t0, 0
+    srai t1, a1, 1
+    addi t2, a1, zero
+
+    Loop:
+    beq t1, t0, end
+
+    lb t3, 0(t0)
+
+    addi t2, t0, zero
+    addi t2, t2, -1
+
+    lb t4, 0(t2)
+    addi t0, t0, 1
+
+    bne t4, t3, neq
+    neq:
+    addi a0, a0, 0
+    ret
+
+    eq:
+    addi t0, t0, 1
+    j Loop
+    
+    end:
+    addi a0, a0, 1
+    ret
