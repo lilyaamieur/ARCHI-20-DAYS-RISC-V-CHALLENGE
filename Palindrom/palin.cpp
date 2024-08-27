23f7cfa9b652f26595881537276fd575efc85bce
Lilya Amieur <146237373+lilyaamieur@users.noreply.github.com>
2024-08-27 08:17:51 +0100

    Update palin.cpp

--- Palindrom/palin.cpp
+++ Palindrom/palin.cpp
@@ -1,1 +1,20 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+using namespace std;
 
+
+int palin(string name){
+    for(int i = 0 ; i < name.length() / 2; i++)
+    {
+        if(name[i] != name[name.length() - i - 1]) return 0;
+    }
+    return 1;
+}
+
+int main() {
+   string name = "lili";
+    int value = palin(name);
+    cout << "the value is " << value << endl;
+    return 0;
+}
