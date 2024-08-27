eceb336dfac5da919c161235a7664c5d53589da2
Lilya Amieur <146237373+lilyaamieur@users.noreply.github.com>
2024-08-27 23:11:57 +0100

    Update BbSort.s

--- BubbleSort/BbSort.s
+++ BubbleSort/BbSort.s
@@ -22,19 +22,18 @@ _start:
 bubblesort:
     li t0, 0            # i = 0
 Loop_i:
-    bge t0, a1, end     # if i >= length, end loop
+    beq t0, a1, end     # if i >= length, end loop
 
-    add t1, t0, zero    # j = i
 Loop_j:
     sub t3, a1, t0      # t3 = length - i
     addi t3, t3, -1    # t3 = length - i - 1
     bge t1, t3, next_i  # if j >= length - i - 1, go to next i
 
-    add t4, a0, t1      # t4 = base address + j
     lb t2, 0(t4)        # load arr[j]
     lb t5, 1(t4)        # load arr[j + 1]
 
     bge t2, t5, no_swap # if arr[j] <= arr[j + 1], no swap
+    
     # Swap arr[j] and arr[j + 1]
     sb t5, 0(t4)        # store arr[j + 1] at arr[j]
     sb t2, 1(t4)        # store arr[j] at arr[j + 1]
@@ -48,4 +47,4 @@ next_i:
     j Loop_i
 
 end:
-    ret
+    jalr zero, 0(ra)
